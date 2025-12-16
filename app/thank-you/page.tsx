8edbd21bb7a4ea21c4ff9975f91bf4dcb08af2f1
Michael Qualizza <mikequalizza@gmail.com>
2025-12-16 13:13:06 -0600

    Add Thank You page component

---
+++ app/thank-you/page.tsx
@@ -0,0 +1,11 @@
+export default function ThankYou() {
+  return (
+    <main>
+      <h1>Thank you.</h1>
+      <p>
+        We are onboarding users in phases.
+        If selected, you’ll receive an invitation by email.
+      </p>
+    </main>
+  )
+}
